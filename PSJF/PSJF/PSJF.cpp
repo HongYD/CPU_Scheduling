43e8cdc470c0b7b303a0b539ce4930640a5610f0
HongYD <nirvanagun93@gmail.com>
2017-11-14 21:03:31 +0900

    PSJF/NSJF

--- PSJF/PSJF/PSJF.cpp
+++ PSJF/PSJF/PSJF.cpp
@@ -4,27 +4,24 @@ typedef struct nonpresjf
 	int at, bt, ft, tat, wt;
 }nonpresjf;
 nonpresjf p[20], p1[20];
+
 int main()
 {
 	int i, limit, nextval, m, min, n;
-	//limit:所有process的运行时间之和
-	//nextval:目前为止最小burst time之和
-	//m:用来存放最小Burst Time的process的编号
-	//n:process数量
-	//min：最小burst time
-	p[0].wt = p[0].tat;
+	p[0].wt = p[0].tat = 0;
+
 	printf("\nEnter the number of process: ");
 	scanf("%d",&n);
-	printf("\n");
+	printf("\n\n");
 	for (i = 1; i <= n; i++)
 	{
-		printf("\nEnter the arrival time for the process P%d: ",i);
+		printf("Enter the arrival time for the process P%d: ", i);
 		scanf("%d",&p[i].at);
 	}
 	limit = p[1].at;
 	for (i = 1; i <= n; i++)
 	{
-		printf("\nEnter the burst time for the process P%d: ", i);
+		printf("Enter the burst time for the process P%d: ",i);
 		scanf("%d", &p[i].bt);
 	}
 	for (i = 1; i <= n; i++)
@@ -35,15 +32,13 @@ int main()
 	{
 		p1[i] = p[i];
 	}
-
-	printf("\n\nGantt chart is as follows: ");
+	printf("\n\nGant chart is as follows:\n\n");
 	printf("%d",p[1].at);
 	nextval = p[1].at;
 	m = 1;
 	do
 	{
 		min = 9999;
-		//当前process的到达时间小于目前最小burst time之和
 		for (i = 1; p[i].at <= nextval&&i <= n; i++)
 		{
 			if (p[i].bt < min&&p[i].bt>0)
@@ -51,9 +46,9 @@ int main()
 				m = i;
 				min = p[i].bt;
 			}
-			nextval += p[m].bt;
-			p[m].bt = 0;
-			printf("->P%d->%d",m,nextval);
+			nextval += 1;
+			p[m].bt = p[m].bt - 1;
+			printf("->P%d->%d", m, nextval);
 			if (p[m].bt == 0)
 			{
 				p[m].ft = nextval;
@@ -63,18 +58,15 @@ int main()
 				p[0].wt += p[m].wt;
 			}
 		}
-	} while (nextval < limit);
-
+	} while (nextval<limit);
 	p[0].tat = p[0].tat / n;
 	p[0].wt = p[0].wt / n;
 
-	printf("\n\n-------------------------TABLE-------------------------\n");
-
+	printf("\n\n-------------------TABLE----------------------------------\n");
 	printf("\nProcess\tAT\tBT\tFT\tTAT\tWT\n");
 	for (i = 1; i <= n; i++)
-	{
-		printf("\nP%d\t%d\t%d\t%d\t%d\t%d\n",i,p[i].at,p1[i].bt,p[i].ft,p[i].tat,p[i].wt);
-	}
-	printf("\n\n-------------------------------------------------------\n");
+		printf("\nP%d\t%d\t%d\t%d\t%d\t%d\n", i, p[i].at, p1[i].bt, p[i].ft, p[i].tat, p[i].wt);
+	printf("\n\n-----------------------------------------------------------\n");
 	return 0;
+
 }
