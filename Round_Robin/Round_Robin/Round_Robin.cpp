0e4cc1123ab3c670e0b4705515ed994b963bc5e7
HongYD <nirvanagun93@gmail.com>
2017-11-15 15:11:54 +0900

    Round Robin

--- Round_Robin/Round_Robin/Round_Robin.cpp
+++ Round_Robin/Round_Robin/Round_Robin.cpp
@@ -1,73 +1,96 @@
-#include<iostream>
-#include<cstdlib>
-#include<queue>
-#include<cstdio>
-using namespace std;
+#include<stdio.h>
 
-typedef struct process
+struct times
 {
-	int id, at, bt, st, ft, pr;
-	float wt, tat;
-}process;
+	int p, art, but, wtt, tat, rnt;
+};
 
-process p[10], p1[10], temp;
-queue<int>q1;
 
-int accept(int ch);
-void turnwait(int n);
-void display(int n);
-void ganttrr(int n);
+void sortart(struct times a[], int pro)
+{
+	int i, j;
+	struct times temp;
+	for (i = 0; i<pro; i++)
+	{
+		for (j = i + 1; j<pro; j++)
+		{
+			if (a[i].art > a[j].art)
+			{
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+	return;
+}
 
 int main()
 {
-	int i, n, ts, ch, j, x;
-	p[0].tat = 0;
-	p[0].wt = 0;
-	n = accept(ch);
-	ganttrr(n);
-	turnwait(n);
-	display(n);
+	int i, j, pro, time, remain, flag = 0, ts;
+	struct times a[100];
+	float avgwt = 0, avgtt = 0;
+	printf("Round Robin Scheduling Algorithm\n");
+	printf("Note -\n1. Arrival Time of at least on process should be 0\n2. CPU should never be idle\n");
+	printf("Enter Number Of Processes : ");
+	scanf("%d", &pro);
+	remain = pro;
+	for (i = 0; i<pro; i++)
+	{
+		printf("Enter arrival time and Burst time for Process P%d : ", i);
+		scanf("%d%d", &a[i].art, &a[i].but);
+		a[i].p = i;
+		a[i].rnt = a[i].but;
+	}
+	sortart(a, pro);
+	printf("Enter Time Slice OR Quantum Number : ");
+	scanf("%d", &ts);
+	printf("\n***************************************\n");
+	printf("Gantt Chart\n");
+	printf("0");
+	for (time = 0, i = 0; remain != 0;)
+	{
+		if (a[i].rnt <= ts && a[i].rnt>0)
+		{
+			time = time + a[i].rnt;
+			printf(" -> [P%d] <- %d", a[i].p, time);
+			a[i].rnt = 0;
+			flag = 1;
+		}
+		else if (a[i].rnt > 0)
+		{
+			a[i].rnt = a[i].rnt - ts;
+			time = time + ts;
+			printf(" -> [P%d] <- %d", a[i].p, time);
+		}
+		if (a[i].rnt == 0 && flag == 1)
+		{
+			remain--;
+			a[i].tat = time - a[i].art;
+			a[i].wtt = time - a[i].art - a[i].but;
+			avgwt = avgwt + time - a[i].art - a[i].but;
+			avgtt = avgtt + time - a[i].art;
+			flag = 0;
+		}
+		if (i == pro - 1)
+			i = 0;
+		else if (a[i + 1].art <= time)
+			i++;
+		else
+			i = 0;
+	}
+	printf("\n\n");
+	printf("***************************************\n");
+	printf("Pro\tArTi\tBuTi\tTaTi\tWtTi\n");
+	printf("***************************************\n");
+	for (i = 0; i<pro; i++)
+	{
+		printf("P%d\t%d\t%d\t%d\t%d\n", a[i].p, a[i].art, a[i].but, a[i].tat, a[i].wtt);
+	}
+	printf("***************************************\n");
+	avgwt = avgwt / pro;
+	avgtt = avgtt / pro;
+	printf("Average Waiting Time : %.2f\n", avgwt);
+	printf("Average Turnaround Time : %.2f\n", avgtt);
 	return 0;
-}
-
-int accept(int ch)
-{
-	int i, n;
-	printf("\nEnter the number of process: ");
-	scanf("%d", &n);
-	if (n == 0)
-	{
-		printf("\nInvalid number of process");
-		exit(1);
-	}
-	for (i = 1; i <= n; i++)
-	{
-		printf("\nEnter the arrival time for process P%d: ",i);
-		scanf("%d",&p[i].at);
-		p[i].id = i;
-	}
-	for (i = 1; i <= n; i++)
-	{
-		printf("\nEnter the burst time for process P%d",i);
-		scanf("%d", &p[i].bt);
-	}
-
-	printf("==============================================================");
-	printf("\nProcess\tAT\tBT");
-	for (i = 1; i <= n; i++)
-	{
-		printf("\nP%d\t%d\t%d",p[i].id,p[i].at,p[i].bt);
-	}
-	printf("==============================================================");
-
-	for (i = 1; i <= n; i++)
-	{
-		p1[i] = p[i];
-		return n;
-	}
-}
-void ganttrr(int n)
-{
-	int i, ts, m, nextval, nextarr;
-
 }
